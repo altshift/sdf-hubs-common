08f035baa028619e318a2bc7bf75ea030c8d4501
as_vnoel <vincent.noel@altshift.fr>
2016-11-08 17:16:20 +0100

    FIA-57 populate all + finalize in default controller

--- api-external/common/defaultController.js
+++ api-external/common/defaultController.js
@@ -4,7 +4,7 @@
 const {
     send400,
     send403,
-    send404,
+    error404,
     handleErrorAsync,
     tooManyArgumentsError
 } = require("./clientError/errorDefinitions");
@@ -16,11 +16,66 @@ const {translateModelsAsync} = require("./translateModels");
  * @param {object} [_options] Options of the generated controller
  * @param {string} [_options.collectionName] the name of the collection
  * @param {object} [_options.s3Config] amazon s3 configs
- * @param {object}  _options.s3Config.translationBucket amazon s3 bucket where to find translation files
- * @param {object}  _options.s3Config.client connected amazon s3 client
+ * @param {function} [_options.finalizeJsonModel] Called with jsonified model data before
+ * being sent to the client
+ * @param {object} _options.s3Config.translationBucket amazon s3 bucket where to find translation files
+ * @param {object} _options.s3Config.client connected amazon s3 client
  * @returns {object} A rest controller with default behaviour
  */
 function generateDefaultController(_options = {}) {
+    /**
+     * @param {object} _model the jsonified model ready to be sent over the wire
+     * @returns {promise<object>} promise returning the transformed object to send
+     */
+    function finalize(_model) {
+        const json = _model.toJSON();
+
+        return new Promise((_resolve, _reject) => {
+            if (typeof _options.finalizeJsonModel === "function") {
+                _options.finalizeJsonModel(json).then(_resolve, _reject);
+            } else {
+                _resolve(json);
+            }
+        });
+    }
+
+    /**
+     * @param {object[]} _models the models to finalize
+     * @returns {promise<object>} promise returning the transformed object to send
+     */
+    function finalizeAll(_models) {
+        return new Promise((_resolve, _reject) => {
+            const allModels = [];
+            const allPromises = _models.map((_model, _index) => {
+                return finalize(_model).then((_json) => {
+                    allModels[_index] = _json;
+                });
+            });
+
+            Promise.all(allPromises)
+                .then(() => {
+                    _resolve(allModels);
+                })
+                .catch(_reject);
+        });
+    }
+
+    /**
+     * @param {string} _404message message to send if 404
+     * @returns {promise<object>} promise returning the query result if no 404
+     */
+    function test404Async(_404message) {
+        return function test404(_queryResult) {
+            const itemIsMissing = _queryResult === undefined;
+
+            if (itemIsMissing) {
+                throw error404(_404message);
+            }
+
+            return _queryResult;
+        };
+    }
+
     /**
      * Send to the client the requested document by id,
      * collection is guessed from url
@@ -51,16 +106,10 @@ function generateDefaultController(_options = {}) {
         const queryPromise = collection.findOne(query);
 
         populate(queryPromise, collection)
-            .then((_result) => {
-                const itemExists = _result;
-
-                if (itemExists) {
-                    _response.send(_result);
-                } else {
-                    send404(_next, `GET: Unable to find item with id '${id}' `
-                        + `from '${_request.asData.collectionName}'`);
-                }
-            })
+            .then(test404Async(`GET: Unable to find item with id '${id}' `
+                        + `from '${_request.asData.collectionName}'`))
+            .then(finalize)
+            .then((_result) => _response.send(_result))
             .catch(handleErrorAsync(_next));
     }
 
@@ -97,6 +146,7 @@ function generateDefaultController(_options = {}) {
                 s3Config.client,
                 s3Config.translationBucket
             ))
+            .then(finalizeAll)
             .then((_result) => {
                 return queryCountPromise.then((_countResult) => {
                     const url = _request.getFullUrl();
@@ -152,9 +202,8 @@ function generateDefaultController(_options = {}) {
         const queryPromise = collection.create(postedItem.value);
 
         populate(queryPromise, collection)
-            .then((_result) => {
-                _response.send(_result);
-            })
+            .then(finalize)
+            .then((_result) => _response.send(_result))
             .catch(handleErrorAsync(_next));
     }
 
@@ -191,13 +240,10 @@ function generateDefaultController(_options = {}) {
         if (collectionHasSoftDelete) {
             collection
                 .update(query, {_isSuppressed: true})
-                .then((_result) => {
-                    if (_result.length > 0) {
-                        _response.status(204).send(); // eslint-disable-line no-magic-numbers
-                    } else {
-                        send404(_next, `Unable to find item with id '${id}' `
-                                            + `from '${collectionName}'`);
-                    }
+                .then(test404Async(`DELETE: Unable to find item with id '${id}' `
+                                            + `from '${collectionName}'`))
+                .then(() => {
+                    _response.status(204).send(); // eslint-disable-line no-magic-numbers
                 })
                 .catch(handleErrorAsync(_next));
         } else {
